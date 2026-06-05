5fe442b0c6b428f4ba600be1816b5dc6bc465264
Miguel Pombo <mp96dev@gmail.com>
2026-06-05 18:51:13 +0100

    feat: add OpenTelemetry into project, fix Grafana, Prometheus and Loki ports mapping

--- src/Services/Catalog.Infrastructure/DependencyInjection.cs
+++ src/Services/Catalog.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,12 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+using Serilog.Enrichers.OpenTelemetry;
 
 namespace Catalog.Infrastructure;
 
@@ -9,10 +16,66 @@ public static class DependencyInjection
 		this IServiceCollection services,
 		IConfiguration configuration)
 	{
-		var checkConnectionString =
-			configuration.GetConnectionString("Database") ?? throw new InvalidOperationException("No Connection String informed");
+		string checkConnectionString =
+			configuration.GetConnectionString("Database") ??
+			throw new InvalidOperationException("No Connection String informed");
 
-		var connectionString = configuration.GetConnectionString(checkConnectionString);
+		string? connectionString = configuration.GetConnectionString(checkConnectionString);
+
+		return services;
+	}
+
+	public static ILoggingBuilder AddLoggingBuilder(
+		this ILoggingBuilder logging,
+		IConfiguration configuration)
+	{
+		string serviceName =
+			configuration["ServiceName"] ?? throw new InvalidOperationException("No Service Name informed");
+
+		Log.Logger = new LoggerConfiguration()
+			.Enrich
+			.WithProperty("service", serviceName)
+			.Enrich
+			.WithEnvironmentName()
+			.Enrich
+			.WithMachineName()
+			.Enrich
+			.WithThreadId()
+			.Enrich
+			.WithOpenTelemetryTraceId()
+			.Enrich
+			.WithOpenTelemetrySpanId()
+			.WriteTo
+			.Console(outputTemplate:
+				"[{Timestamp:HH:mm:ss} {Level:u3}] [{MachineName}] {Message:lj}{NewLine}{Exception}")
+			.CreateLogger();
+
+		logging.AddOpenTelemetry(options =>
+		{
+			options.SetResourceBuilder(
+					ResourceBuilder.CreateDefault()
+						.AddService(serviceName))
+				.AddConsoleExporter();
+		});
+
+		return logging;
+	}
+
+	public static IServiceCollection AddTelemetryServices(
+		this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		string serviceName =
+			configuration["ServiceName"] ?? throw new InvalidOperationException("No Service Name informed");
+
+		services.AddOpenTelemetry()
+			.ConfigureResource(resource => resource.AddService(serviceName))
+			.WithTracing(tracing => tracing
+				.AddAspNetCoreInstrumentation()
+				.AddConsoleExporter())
+			.WithMetrics(metrics => metrics
+				.AddAspNetCoreInstrumentation()
+				.AddConsoleExporter());
 
 		return services;
 	}
