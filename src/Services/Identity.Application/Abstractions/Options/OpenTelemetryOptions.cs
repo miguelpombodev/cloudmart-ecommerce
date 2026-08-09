c8ee3375bf99d4d8864d3995dce77e664e55dc44
Miguel Pombo <mp96dev@gmail.com>
2026-08-09 19:45:39 +0100

    refactor: create OpenTelemetryOptions and add more attributes for ExternalProvider class

---
+++ src/Services/Identity.Application/Abstractions/Options/OpenTelemetryOptions.cs
@@ -0,0 +1,19 @@
+namespace Identity.Application.Abstractions.Options;
+
+
+public sealed class KeyValueOptions
+{
+  public string Key { get; init; } = string.Empty;
+
+  public object Value { get; init; } = new();
+}
+
+public sealed class OpenTelemetryOptions
+{
+  public string ApplicationName { get; init; } = string.Empty;
+  public string ServiceName { get; init; } = string.Empty;
+
+  public string OtelUrl { get; init; } = string.Empty;
+
+  public IEnumerable<KeyValueOptions> OtelAttributes { get; init; } = [];
+}
