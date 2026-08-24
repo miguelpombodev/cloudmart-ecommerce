52f648f7303fd3c6fd57a2fe3ed7a6094424ab52
Miguel Pombo <mp96dev@gmail.com>
2026-08-24 19:53:39 +0100

    feat: add GetInformationsEndpoint to retrieve logged user data

---
+++ src/Services/Identity.Application/Features/Users/GetInformations/GetInformationsQueryHandler.cs
@@ -0,0 +1,32 @@
+using BuildingBlocks.Abstractions;
+using BuildingBlocks.CQRS;
+using Identity.Application.Abstractions.Repositories;
+using Identity.Domain.Entities;
+
+namespace Identity.Application.Features.Users.GetInformations;
+
+public sealed class GetInformationsQueryHandler: IQueryHandler<GetInformationsQuery, Result<GetInformationsResponse>>
+{
+
+  private readonly IUserRepository _repository;
+
+  public GetInformationsQueryHandler(IUserRepository repository)
+  {
+    _repository = repository;
+  }
+
+
+  public async Task<Result<GetInformationsResponse>> Handle(GetInformationsQuery request, CancellationToken cancellationToken)
+  {
+    User? checkUser = await _repository.FindByIdAsync(request.userId, cancellationToken);
+
+    if (checkUser is null)
+    {
+      return Result<GetInformationsResponse>.Failure(Error.Conflict("User not registered!"));
+    }
+
+    var user = new GetInformationsResponse(checkUser.Name.ToString(), checkUser.CreatedAt);
+
+    return Result<GetInformationsResponse>.Success(user);
+  }
+}
