27ed3adaab57a69e8638d2e886c3ae4e57299195
Miguel Pombo <mp96dev@gmail.com>
2026-08-28 13:41:47 +0100

    chore: add documentation for get antiforgery token endpoint

--- src/Services/Identity.API/Features/AntiForgery.cs
+++ src/Services/Identity.API/Features/AntiForgery.cs
@@ -10,13 +10,17 @@ public sealed class AntiForgery : ICarterModule
   public void AddRoutes(IEndpointRouteBuilder app)
   {
     app.MapGet("/antiforgery/token", (
-      HttpContext context,
-      IAntiforgery antiforgery) =>
-    {
-      AntiforgeryTokenSet tokens = antiforgery.GetAndStoreTokens(context);
+        HttpContext context,
+        IAntiforgery antiforgery) =>
+      {
+        AntiforgeryTokenSet tokens = antiforgery.GetAndStoreTokens(context);
 
-      return Results.Ok(new { token = tokens.RequestToken });
-    })
-      .AllowAnonymous();
+        return Results.Ok(new { token = tokens.RequestToken });
+      }).AllowAnonymous()
+      .WithName("GetAntiForgeryToken")
+      .Produces(StatusCodes.Status200OK)
+      .ProducesProblem(StatusCodes.Status401Unauthorized)
+      .ProducesProblem(StatusCodes.Status404NotFound)
+      .WithSummary("Get AntiForgery Token for multipart-form");
   }
 }
