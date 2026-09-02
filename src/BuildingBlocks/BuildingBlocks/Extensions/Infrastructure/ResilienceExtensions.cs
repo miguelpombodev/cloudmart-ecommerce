84383fa53d686693ffe17784ea6f89e39ddd825c
Miguel Pombo <mp96dev@gmail.com>
2026-09-02 15:44:33 +0100

    feat: create extension methods in BuildingBlocks for resilience for all services

---
+++ src/BuildingBlocks/BuildingBlocks/Extensions/Infrastructure/ResilienceExtensions.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using Polly;
+using Polly.Retry;
+
+namespace BuildingBlocks.Extensions.Infrastructure;
+
+public static class ResilienceExtensions
+{
+  public static IServiceCollection AddNonHttpResilience(this IServiceCollection services, IConfiguration configuration)
+  {
+    services.AddResiliencePipeline("database-operations", builder =>
+    {
+      builder
+        .AddTimeout(TimeSpan.FromSeconds(30))
+        .AddRetry(new RetryStrategyOptions
+        {
+          MaxRetryAttempts = 2,
+          Delay = TimeSpan.FromMicroseconds(500),
+          BackoffType = DelayBackoffType.Exponential,
+          UseJitter = true,
+          ShouldHandle = new PredicateBuilder()
+            .Handle<TimeoutException>()
+            .Handle<NpgsqlException>(ex => ex.IsTransient)
+        });
+    });
+
+    return services;
+  }
+}
